4fa003a7a09cc259971c9ce02f22d8f199946fda
ernie.lee <ernie.lee@ubitusinc.com>
2024-08-13 11:28:11 +0800

    initialize project

---
+++ src/main/java/com/example/rabbitmq_chatroom_demo/RabbitmqChatroomDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.rabbitmq_chatroom_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RabbitmqChatroomDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RabbitmqChatroomDemoApplication.class, args);
+	}
+
+}
